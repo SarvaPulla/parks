658193596ccfcff88102e096db2a2eb033db8c14
sarva <sarvateja@gmail.com>
2015-07-28 17:58:46 -0600

    Measured distance

--- SarvaPulla.github.io/parks.js
+++ SarvaPulla.github.io/parks.js
@@ -649,3 +649,53 @@ osm_geojson.osm2geojson = function(osm, metaProperties) {
 if (typeof module !== 'undefined') module.exports = osm_geojson;
 
 L.Control.ZoomDisplay=L.Control.extend({options:{position:"topright"},onAdd:function(a){return this._map=a,this._container=L.DomUtil.create("div","leaflet-control-zoom-display leaflet-bar-part leaflet-bar"),this.updateMapZoom(a.getZoom()),a.on("zoomend",this.onMapZoomEnd,this),this._container},onRemove:function(a){a.off("zoomend",this.onMapZoomEnd,this)},onMapZoomEnd:function(){this.updateMapZoom(this._map.getZoom())},updateMapZoom:function(a){this._container.innerHTML="The current zoom level is "+a}}),L.Map.mergeOptions({zoomDisplayControl:!0}),L.Map.addInitHook(function(){this.options.zoomDisplayControl&&(this.zoomDisplayControl=new L.Control.ZoomDisplay,this.addControl(this.zoomDisplayControl))}),L.control.zoomDisplay=function(a){return new L.Control.ZoomDisplay(a)};
+
+/*
+ * Extends L.Polyline to retrieve measured distance.
+ *
+ * https://github.com/danimt/Leaflet.PolylineMeasuredDistance
+ */
+
+L.Polyline.prototype.measuredDistance = function (options) {
+  // Default options
+  this.defaults = {
+    metric: true
+  };
+  // Extend options
+  this.options = L.extend(this.defaults, options);
+
+  var distance = 0,  coords = null, coordsArray = this._latlngs;
+
+  for (i = 0; i < coordsArray.length - 1; i++) {
+    coords = coordsArray[i];
+    distance += coords.distanceTo(coordsArray[i + 1]);
+  }
+
+  // Return formatted distance
+  return L.PolylineUtil.readableDistance(distance, this.options.metric);
+};
+
+L.PolylineUtil = {
+  readableDistance: function (distance, isMetric) {
+    var distanceStr;
+
+    if (isMetric) {
+      // show metres when distance is < 1km, then show km
+      if (distance > 1000) {
+        distanceStr = (distance  / 1000).toFixed(2) + ' km';
+      } else {
+        distanceStr = Math.ceil(distance) + ' m';
+      }
+    } else {
+      distance *= 1.09361;
+
+      if (distance > 1760) {
+        distanceStr = (distance / 1760).toFixed(2) + ' miles';
+      } else {
+        distanceStr = Math.ceil(distance) + ' yd';
+      }
+    }
+
+    return distanceStr;
+  }
+};
